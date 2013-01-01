7c1871d4f2893af65ac22c64aeb5ef06bcd069dd
Nathan Kot <me@nathankot.com>
2013-01-01 22:46:54 +1300

    Add functionality to run all core tests.

--- _cli/commands/test.php
+++ _cli/commands/test.php
@@ -5,7 +5,26 @@ line();
 
 $class_name = str_replace(array('_', '/'), '\\', $argv[2]);
 
+if ($class_name === 'core') {
+	$non_tested = ['mock'];
+	foreach (glob(ROOT.'/Vendor/Core/Test/*/*.php') as $file) {
+		preg_match('@\/Vendor\/Core\/Test\/(.*)\/(.*)\.php@', $file, $file_parts);
+		$class_name = "\\Core\\Test\\{$file_parts[1]}\\{$file_parts[2]}";
+		if (in_array(strtolower($file_parts[1]), $non_tested)) {
+			continue;
+		}
+		test($class_name);
+	}
+	return;
+}
+
 try {
+	test($class_name);
+} catch (\RuntimeException $e) {
+	error($e->getMessage());
+}
+
+function test($class_name) {
 	if (class_exists($class_name)) {
 		message("Testing {$class_name}");
 		line();
@@ -15,6 +34,4 @@ try {
 	} else {
 		error("Class {$class_name} does not exist!");
 	}
-} catch (\RuntimeException $e) {
-	error($e->getMessage());
 }
