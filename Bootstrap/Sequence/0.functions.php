6f00b42738deaea5d924a69bb13ba48449454094
Nathan Kot <me@nathankot.com>
2013-01-15 03:10:08 +1300

    Formatting.

--- Bootstrap/Sequence/0.functions.php
+++ Bootstrap/Sequence/0.functions.php
@@ -9,16 +9,16 @@
  * @see  http://bit.ly/S2FEWr
  **/
 function extend($a, $b) {
-    foreach($b as $k=>$v) {
-        if( is_array($v) ) {
-            if( !isset($a[$k]) ) {
-                $a[$k] = $v;
-            } else {
-                $a[$k] = extend($a[$k], $v);
-            }
-        } else {
-            $a[$k] = $v;
-        }
-    }
-    return $a;
+		foreach($b as $k=>$v) {
+				if( is_array($v) ) {
+						if( !isset($a[$k]) ) {
+								$a[$k] = $v;
+						} else {
+								$a[$k] = extend($a[$k], $v);
+						}
+				} else {
+						$a[$k] = $v;
+				}
+		}
+		return $a;
 }
